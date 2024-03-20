de5f89315bc405ac976c5f8ab4e50021988c293f
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-20 23:43:26 +0000

    TASK 1

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
 """ Test suite for the console"""
-
+import json
+import MySQLdb
+import sqlalchemy
 import unittest
 import os
-import sys
 import models
 from models import storage
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
 from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
-from unittest.mock import create_autospec
 
 
 class TestHBNBCommand(unittest.TestCase):
     """Test the console module"""
-    def setUp(self):
-        """Set up for tests"""
-        self.cmd = HBNBCommand()
-
     @unittest.skipIf(
         os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
     def test_fs_create(self):
         """Tests the create method with the file storage."""
-        self.cmd.do_create("Base name=\"example\" age=25")
-        created_instance = self.cmd.storage.all().values()
-        self.assertTrue(len(created_instance) == 1)
-        instance = created_instance[0]
-        self.assertEqual(instance.__class__.__name__, "Base")
-        self.assertEqual(instance.name, "example")
-        self.assertEqual(instance.age, 25)
-
-    def tearDown(self):
-        """Clean up storage after each test"""
-        self.cmd.storage.reset()
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            cons.onecmd('create City name="Texas"')
+            mdl_id = cout.getvalue().strip()
+            clear_stream(cout)
+            self.assertIn('City.{}'.format(mdl_id), storage.all().keys())
+            cons.onecmd('show City {}'.format(mdl_id))
+            self.assertIn("'name': 'Texas'", cout.getvalue().strip())
+            clear_stream(cout)
+            cons.onecmd('create User name="James" age=17 height=5.9')
+            mdl_id = cout.getvalue().strip()
+            self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
+            clear_stream(cout)
+            cons.onecmd('show User {}'.format(mdl_id))
+            self.assertIn("'name': 'James'", cout.getvalue().strip())
+            self.assertIn("'age': 17", cout.getvalue().strip())
+            self.assertIn("'height': 5.9", cout.getvalue().strip())
