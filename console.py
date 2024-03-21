4d2f29de8a6926d9ae7522f5b9cd2c3168766e26
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-21 00:02:31 +0000

    TASK 1

--- console.py
+++ console.py
@@ -147,8 +147,8 @@ class HBNBCommand(cmd.Cmd):
                 class_attributes[key] = value
 
         new_instance = HBNBCommand.classes[class_name](**class_attributes)
-        storage.save()
         print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
