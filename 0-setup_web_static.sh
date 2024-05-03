0915841e76f6da48d6d524fd0ba340a54fc2039e
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-03 22:16:02 +0000

    Task 0 - Deploy Static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo chown -R ubuntu:ubuntu /data/
+
+echo "<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>This is a test page</h1>
+    <p>Holberton School</p>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo sed -i '/hbnb_static/ d' /etc/nginx/sites-available/default
+sudo sed -i 's/^\tlocation \/ {$/\
+\tlocation \/hbnb_static\/ {\
+\t\talias \/data\/web_static\/current\/;\
+\t}\
+\n\n\tlocation \/\ {$/' /etc/nginx/sites-available/default
+
+sudo service nginx restart
