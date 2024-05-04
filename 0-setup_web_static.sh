c3cc71cdd0594d19029175a0adc2b4d509d3d0f7
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-04 00:00:26 +0000

    Task 0 - Deploy Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,6 +22,6 @@ echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo sed -i '/hbnb_static/ d' /etc/nginx/sites-available/default
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
