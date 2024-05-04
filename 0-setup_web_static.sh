33f346e299bfedcfb7f93020b7dd48fe418d4955
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-04 00:05:45 +0000

    Task 0 - Deploy Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo chown -R ubuntu:ubuntu /data/
