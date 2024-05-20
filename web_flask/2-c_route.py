e8aedfdaf4e511ae2e69362454b21b1dc580529d
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-20 00:40:14 +0000

    Task 1 - HBNB{Modified} & Task 2 - C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,12 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Replace underscores with spaces"""
+    text = unquote(text.replace('_', ' '))
+    return 'C {}'.format(text)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
